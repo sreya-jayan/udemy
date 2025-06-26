3a77d57020a2e3139ef580ef9dc55d553c3c3df4
sreya-jayan <sreyajayan35@gmail.com>
2025-06-26 14:50:28 +0530

    Removed udemy/src from Git and added to .gitignore

--- udemy/src/udemy/api/v1/courses/pagination.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import pagination
-
-
-class StandardResultSetPagination(pagination.PageNumberPagination):
-    page_size = 2
-    page_query_param = "page"
-    page_size_query_param = "page_size"
-    max_page_size = 10
--- udemy/src/udemy/courses/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from courses.models import Course,Category,Purchase,User
-
-
-admin.site.register(Course)
-admin.site.register(Category)
-admin.site.register(Purchase)
-admin.site.register(User)
